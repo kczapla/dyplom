71ceaef17a2b8c6f3ffe3b6dc0b5b9f8a7cd2fcf
Krzysztof Czapla <k.czapla6@hotmail.com>
2014-12-18 21:01:00 +0100

    reset

---
+++ gui/buttons/quitter.py
@@ -0,0 +1,31 @@
+__author__ = 'perun'
+
+#!/usr/bin/python3
+
+"""
+a Quit button that verifies exit requests;
+to reuse, attach an instance to other GUIs, and re-pack as desired
+"""
+
+from tkinter import *
+from tkinter.messagebox import *
+
+
+class Quitter(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack()
+        widget = Button(self, text='Quit', command=self.quit)
+        widget.pack(side=LEFT, expand=YES, fill=BOTH)
+
+    def quit(self):
+        ans = askokcancel('Verify exit', 'Really quit?')
+        if ans:
+            Frame.quit(self)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    q = Quitter(root)
+    q.pack(side=LEFT)
+    q.mainloop()
